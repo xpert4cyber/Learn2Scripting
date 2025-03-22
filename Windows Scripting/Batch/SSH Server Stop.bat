fc3a7fe26027ed5ea7550b5ecee1c75294bef1f7
Shubham Chaudhary <66991429+xpert4cyber@users.noreply.github.com>
2025-03-22 12:32:38 +0530

    Delete Windows Scripting/Batch directory

--- Windows Scripting/Batch/SSH Server Stop.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Stopping SSH Server...
-net stop sshd
-if %errorlevel%==0 (
-    echo SSH Server stopped successfully.
-) else (
-    echo Failed to stop SSH Server or it is not running.
-)
-pause
